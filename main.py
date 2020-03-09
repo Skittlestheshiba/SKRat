68ce15e89ab57850435fae1f2212ff3c24af8fd0
Sukittoruzu <bbllee33001177@gmail.com>
2020-03-09 13:58:49 -0400

    Comments.

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+#Written by Skittles_
+
+#Imports
 import colors
 import sys
 import os
 import server
 
+#Functions
 def menu():
     print(colors.blue('|--SKRat--|') + '\n \n \n \n \n \n \n' + colors.blue('1) Option 1  2) Option 2 E) Exit'))
 
+#Variables
 selection = input(colors.blue('>'))
+
 
 menu()
 if selection == '1':
     print(colors.red('1'))
-    
+
 elif selection == '2':
     print(colors.red('2'))
 
@@ -26,3 +32,4 @@ else:
         os.system('clear')
 
     menu()
+#Written by Skittles_
